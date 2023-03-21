ccd83861ab211d48bc47597ab6a3499c68c94575
Martin Stoyanov <martin.stoyanov@vertalo.com>
2023-03-21 13:52:11 -0500

    removed some stuff

--- scripts/version.sh
+++ scripts/version.sh
@@ -25,7 +25,7 @@ export full_opam_repository_tag=83a6f7bc4a32e413840cf4a20afb1557fca38a5e
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab/ci/templates.yml
 export opam_repository_url=https://github.com/Vertalo/opam-repository
-export opam_repository_tag="${OPAM_REPOSITORY_TAG:-8892b007a879ad190a8fe9c5e3bc39a85558010b}"
+export opam_repository_tag="${OPAM_REPOSITORY_TAG:-44c9ca54e49ec7da074a3c7759c69a1c11a1843d}"
 export opam_repository_git="$opam_repository_url.git"
 export opam_repository="$opam_repository_git"\#"$opam_repository_tag"
 
