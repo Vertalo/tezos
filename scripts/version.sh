1ad201561791b84dcaf4d00c60e704acd5de799b
Martin Stoyanov <martin.stoyanov@vertalo.com>
2024-05-28 11:44:23 -0500

    hacl star woes

--- scripts/version.sh
+++ scripts/version.sh
@@ -26,7 +26,7 @@ export alpine_version='3.18'
 
 ## full_opam_repository is a commit hash of the public OPAM repository, i.e.
 ## https://github.com/ocaml/opam-repository
-export full_opam_repository_tag=518f55a1ee5da870035b9593f98db03f43ce7f5f
+export full_opam_repository_tag=f09a88ef5219ec23700b8274a6f8a91d4d2cf8cc
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be reflected in the `build_deps_image_version` variable
